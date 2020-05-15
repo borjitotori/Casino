43989e050b306915eea0f846d7d0addc642f2e2f
santiago moncada <santiago.m.a@hotmail.com>
2020-05-15 20:08:16 +0200

    Updates

---
+++ precache-manifest.7ec63f5cb25652c504508f0ff8dca8ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7dfd2dbaf82ee242846b4c1e452d7e",
+    "url": "/Casino/index.html"
+  },
+  {
+    "revision": "4930949d135bbb90e893",
+    "url": "/Casino/static/css/main.024eb4ad.chunk.css"
+  },
+  {
+    "revision": "e058317f9e8d0eae36d0",
+    "url": "/Casino/static/js/2.11315b5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Casino/static/js/2.11315b5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4930949d135bbb90e893",
+    "url": "/Casino/static/js/main.2518dd60.chunk.js"
+  },
+  {
+    "revision": "b1998fba4046753b2a0c",
+    "url": "/Casino/static/js/runtime-main.6febf6a4.js"
+  },
+  {
+    "revision": "9834fc2dfaeb369b4d7bc5685e3a1cfd",
+    "url": "/Casino/static/media/Blackjack-Table-1-scaled.9834fc2d.jpg"
+  },
+  {
+    "revision": "bd2005cc3752ec2b694f2171fd1ceaf3",
+    "url": "/Casino/static/media/Las_Vegas_slot_machines.bd2005cc.jpg"
+  },
+  {
+    "revision": "1b6e61dac15ff2b07881acdd31ee31cc",
+    "url": "/Casino/static/media/roulette.1b6e61da.jpg"
+  }
+]);
